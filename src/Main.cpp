343e8962fea9852cfaee4a00a56a424871e66b52
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2015-02-13 14:34:37 +0100

    Some more code structures and debug print

--- src/Main.cpp
+++ src/Main.cpp
@@ -9,6 +9,7 @@
  */
 
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <vector>
 #include <algorithm>
@@ -16,9 +17,34 @@
 // TODO(SRombauts) add a structure for orientation/direction data
 // 'H'orizontal or 'V'ertical orientation
 
-// TODO(SRombauts) add a class for commands (+text messages)
-// LEFT, RIGHT, UP, DOWN
-// or "putX putY putOrientation"
+/**
+ * @brief Send commands to the game thru standard output
+ * 
+ * Commands are string like LEFT, RIGHT, UP, DOWN or "putX putY putOrientation".
+ */
+class Command {
+public:
+    /// Move the player to the right of the board (x++)
+    static void right(const char* apMessage) {
+        std::cout << "RIGHT " << apMessage << std::endl;
+    }
+    /// Move the player to the left of the board (x--)
+    static void left(const char* apMessage) {
+        std::cout << "LEFT " << apMessage  << std::endl;
+    }
+    /// Move the player to the bottom of the board (y++)
+    static void down(const char* apMessage) {
+        std::cout << "DOWN " << apMessage  << std::endl;
+    }
+    /// Move the player to the top of the board (y--)
+    static void up(const char* apMessage) {
+        std::cout << "UP " << apMessage  << std::endl;
+    }
+    /// Put a new Wall to a specified location and orientation
+    static void put(const size_t aX, const size_t aY, const char aOrientation, const char* apMessage) {
+        std::cout << aX << aY << aOrientation << " " << apMessage  << std::endl;
+    }
+};
 
 /// coordinates
 struct Coords {
@@ -39,6 +65,9 @@ struct Player {
 
 /// wall data
 struct Wall {
+    /// Vector of walls
+    typedef std::vector<Wall> Vector;
+
     Coords coords;      ///< coordinates of the upper left corner of the wall
     char   orientation; ///< 'H'orizontal or 'V'ertical orientation
 };
@@ -54,16 +83,39 @@ template <typename TElement>
 class Matrix {
 public:
     /**
-    * ctor allocating the matrix to the specified size
-    *
-    * @param aWidthX    Nb of columns (X coordinate)
-    * @param aHeightY   Nb of lines   (Y coordinate)
-    */
+     * ctor allocating the matrix to the specified size (with default initialisation)
+     *
+     * @param aWidthX    Nb of columns (X coordinate)
+     * @param aHeightY   Nb of lines   (Y coordinate)
+     */
     Matrix(const size_t aWidthX, const size_t aHeightY) {
         matrix.resize(aWidthX);
         for (auto& line : matrix) {
             line.resize(aHeightY);
         }
+    }
+
+    /**
+     * ctor allocating the matrix to the specified size with explicit initialisation
+     *
+     * @param aWidthX    Nb of columns (X coordinate)
+     * @param aHeightY   Nb of lines   (Y coordinate)
+     * @param aInitValue Initial Value of every elements
+     */
+    Matrix(const size_t aWidthX, const size_t aHeightY, const TElement& aInitValue) {
+        matrix.resize(aWidthX);
+        for (auto& line : matrix) {
+            line.resize(aHeightY, aInitValue);
+        }
+    }
+
+    /// width of the matrix (Nb of columns, X axis)
+    size_t width() const {
+        return matrix.size();
+    }
+    /// height of the matrix (Nb of lines, Y axis)
+    size_t height() const {
+        return matrix.at(0).size();
     }
 
     /// getter for cell at [X, Y] (const reference) uses vector::at() with safety check : can throw std::out_of_range
@@ -75,22 +127,6 @@ public:
         return matrix.at(aX).at(aY);
     }
 
-    /// debug:
-    void dump() const {
-        std::cerr << "  |  ";
-        for (size_t x = 0; x < matrix.size(); ++x) {
-            std::cerr << x << "  |  ";
-        }
-        std::cerr << std::endl;
-        for (size_t y = 0; y < matrix[0].size(); ++y) {
-            std::cerr << y << " | ";
-            for (size_t x = 0; x < matrix.size(); ++x) {
-                std::cerr << matrix[x][y].weight << " " << matrix[x][y].direction << " | ";
-            }
-            std::cerr << std::endl;
-        }
-    }
-
 private:
     /// Vector of Elements
     typedef std::vector<TElement>   Vector1D;
@@ -99,6 +135,24 @@ private:
 
     Vector2D matrix;    ///< Matrix as a vector of vectors of Elements
 };
+
+/// debug: dump content of the Matrix of Cells
+void dump(const Matrix<Cell>& aMatrix) {
+    std::cerr << "  |  ";
+    for (size_t x = 0; x < aMatrix.width(); ++x) {
+        std::cerr << x << "  |  ";
+    }
+    std::cerr << std::endl;
+    for (size_t y = 0; y < aMatrix.height(); ++y) {
+        std::cerr << y << " |";
+        for (size_t x = 0; x < aMatrix.width(); ++x) {
+            std::cerr << std::fixed << std::setprecision(1) << aMatrix.get(x, y).weight
+                      << " " << aMatrix.get(x, y).direction << "|";
+        }
+        std::cerr << std::endl;
+    }
+}
+
 
 /**
  * Auto-generated code below aims at helping you parse
@@ -116,7 +170,7 @@ int main() {
     size_t turn = 0;
 
     // TODO(SRombauts) test a pathfinding algorithm using the grid
-    Matrix<Cell> grid(w, h);
+    Matrix<Cell> grid(w, h, Cell{1.0, 0});
 
     // all players statuses
     Player::Vector players(playerCount);
@@ -125,25 +179,24 @@ int main() {
     // game loop
     while (1) {
         for (size_t id = 0; id < playerCount; ++id) {
+            players[id].id          = id; // redundant, but useful for debug
+
             int x; // x-coordinate of the player
             int y; // y-coordinate of the player
-            size_t wallsLeft; // number of walls available for the player
-            std::cin >> x >> y >> wallsLeft; std::cin.ignore();
+            std::cin >> x >> y >> players[id].wallsLeft; std::cin.ignore();
 
-            players[id].id          = id; // redundant, but useful for debug
-            players[id].wallsLeft   = wallsLeft;
-            // if player still playing, add it to the list of players
+            // if player still playing
             if ((x >= 0) && (y >= 0)) {
                 players[id].coords.x = static_cast<size_t>(x);
                 players[id].coords.y = static_cast<size_t>(y);
                 players[id].bIsAlive = true;
 
-                // set the player on the grid
-                grid.set(players[id].coords.x, players[id].coords.y);
+                // set the player on the grid : 0.0 weight to forbid this cell
+                grid.set(players[id].coords.x, players[id].coords.y).weight = 0.0;
 
                 // debug:
                 if (id == myId) {
-                    std::cerr << "me(" << players[id].id << "): [" << players[id].coords.x
+                    std::cerr << "myself(" << players[id].id << "): [" << players[id].coords.x
                               << ", " << players[id].coords.y << "] (left=" << players[id].wallsLeft << ")\n";
                 } else {
                     std::cerr << "player(" << players[id].id << "): [" << players[id].coords.x
@@ -151,31 +204,29 @@ int main() {
                 }
             } else {
                 players[id].bIsAlive = false;
-                std::cerr << "dead(" << id << "): [" << x << ", " << y << "]\n";
+                std::cerr << "_dead_(" << id << "): [" << x << ", " << y << "]\n";
             }
         }
 
         // debug:
-        grid.dump();
+        dump(grid);
 
-        size_t wallCount; // number of walls on the board
+        size_t          wallCount; // number of walls on the board
         std::cin >> wallCount; std::cin.ignore();
+        Wall::Vector    walls(wallCount);
         for (size_t wall = 0; wall < wallCount; ++wall) {
-            int wallX; // x-coordinate of the wall
-            int wallY; // y-coordinate of the wall
-            std::string wallOrientation; // wall orientation ('H' or 'V')
-            std::cin >> wallX >> wallY >> wallOrientation; std::cin.ignore();
-            std::cerr << "wall(" << wall << "): [" << wallX << ", " << wallY << "]\n";
+            std::cin >> walls[wall].coords.x >> walls[wall].coords.y >> walls[wall].orientation; std::cin.ignore();
+            std::cerr << "wall(" << wall << "): [" << walls[wall].coords.x << ", " << walls[wall].coords.y << "] '"
+                      << walls[wall].orientation <<"'\n";
         }
-
-        // Write an action using std::cout. DON'T FORGET THE "<< std::endl"
-        // To debug: std::cerr << "Debug messages..." << std::endl;
 
         ++turn;
         std::cerr << "turn " << turn << std::endl;
 
         // action: LEFT, RIGHT, UP, DOWN or "putX putY putOrientation" to place a wall
-        std::cout << "RIGHT" << std::endl;
+        Command::right("go go go!");
+
+        // return 1; // for debug purpose
     }
 
     return 0;
