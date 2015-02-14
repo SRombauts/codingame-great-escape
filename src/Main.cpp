822dd7081826d305533e3afcd70e89e90d598664
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2015-02-14 21:15:36 +0100

    Add walls to a collision detection matrix

--- src/Main.cpp
+++ src/Main.cpp
@@ -16,9 +16,6 @@
 #include <limits>
 #include <stdexcept>
 
-// TODO(SRombauts) add a structure for orientation/direction data
-// 'H'orizontal or 'V'ertical orientation
-
 /// Define directions
 enum EDirection {
     eRight,
@@ -41,7 +38,7 @@ public:
             right("go go go!");
             break;
         case eLeft:
-            left("back");
+            left("back home");
             break;
         case eDown:
             down("down the path...");
@@ -80,6 +77,32 @@ public:
 struct Coords {
     size_t x;   ///< x-coordinate (column)
     size_t y;   ///< y-coordinate (row/line)
+
+    /// get coordinates of the next cell at the right of the current one
+    Coords right() const {
+        return Coords{ x + 1, y };
+    }
+    /// get coordinates of the next cell at the left of the current one
+    Coords left() const {
+        return Coords{ x - 1, y };
+    }
+    /// get coordinates of the next cell at the bottom of the current one
+    Coords down() const {
+        return Coords{ x, y + 1 };
+    }
+    /// get coordinates of the next cell at the top of the current one
+    Coords up() const {
+        return Coords{ x, y - 1 };
+    }
+
+    /// get coordinates of the next cell at the top-right of the current one
+    Coords upright() const {
+        return Coords{ x + 1, y - 1 };
+    }
+    /// get coordinates of the next cell at the bottom-left of the current one
+    Coords downleft() const {
+        return Coords{ x - 1, y + 1 };
+    }
 };
 
 /// player data
@@ -93,7 +116,7 @@ struct Player {
     bool   bIsAlive;    ///< true while the player is alive
 };
 
-/// wall data
+/// wall data for the list of walls
 struct Wall {
     /// Vector of walls
     typedef std::vector<Wall> Vector;
@@ -102,7 +125,15 @@ struct Wall {
     char   orientation; ///< 'H'orizontal or 'V'ertical orientation
 };
 
-/// pathfinding data for a cell of the grid
+/// wall collision data for the path of walls
+struct Collision {
+    bool bRight;    ///< is there a wall on the right of this Cell
+    bool bLeft;     ///< is there a wall on the left of this Cell
+    bool bDown;     ///< is there a wall on the bottom of this Cell
+    bool bUp;       ///< is there a wall on the top of this Cell
+};
+
+/// pathfinding data for a cell of the path
 struct Cell {
     float       weight;     ///< weighted distance toward the destination
     EDirection  direction;  ///< direction of the shortest/best path
@@ -122,8 +153,8 @@ public:
      * @param aHeightY   Nb of lines   (Y coordinate)
      */
     Matrix(const size_t aWidthX, const size_t aHeightY) {
-        matrix.resize(aWidthX);
-        for (auto& line : matrix) {
+        mMatrix.resize(aWidthX);
+        for (auto& line : mMatrix) {
             line.resize(aHeightY);
         }
     }
@@ -136,28 +167,36 @@ public:
      * @param aInitValue Initial Value of every elements
      */
     Matrix(const size_t aWidthX, const size_t aHeightY, const TElement& aInitValue) {
-        matrix.resize(aWidthX);
-        for (auto& line : matrix) {
+        mMatrix.resize(aWidthX);
+        for (auto& line : mMatrix) {
             line.resize(aHeightY, aInitValue);
         }
     }
 
     /// width of the matrix (Nb of columns, X axis)
     size_t width() const {
-        return matrix.size();
+        return mMatrix.size();
     }
     /// height of the matrix (Nb of lines, Y axis)
     size_t height() const {
-        return matrix.at(0).size();
+        return mMatrix.at(0).size();
     }
 
     /// getter for cell at [X, Y] (const reference) uses vector::at() with safety check : can throw std::out_of_range
     const TElement& get(const size_t aX, const size_t aY) const {
-        return matrix.at(aX).at(aY); // return matrix[aX][aY];
+        return mMatrix.at(aX).at(aY); // return mMatrix[aX][aY];
+    }
+    /// getter for cell at [X, Y] (const reference) uses vector::at() with safety check : can throw std::out_of_range
+    const TElement& get(const Coords& aCoords) const {
+        return mMatrix.at(aCoords.x).at(aCoords.y);
     }
     /// "setter" for cell at [X, Y] (reference) uses vector::at() with safety check : can throw std::out_of_range
     TElement& set(const size_t aX, const size_t aY) {
-        return matrix.at(aX).at(aY);
+        return mMatrix.at(aX).at(aY);
+    }
+    /// "setter" for cell at [X, Y] (reference) uses vector::at() with safety check : can throw std::out_of_range
+    TElement& set(const Coords& aCoords) {
+        return mMatrix.at(aCoords.x).at(aCoords.y);
     }
 
 private:
@@ -166,29 +205,49 @@ private:
     /// Matrix as a vector of vectors of Elements
     typedef std::vector<Vector1D>   Vector2D;
 
-    Vector2D matrix;    ///< Matrix as a vector of vectors of Elements
+    Vector2D mMatrix;    ///< Matrix as a vector of vectors of Elements
 };
 
-/// TODO(SRombats): testing shortest path algorithm
-void shortest(Matrix<Cell>& aMatrix, const size_t aX, const size_t aY, const float aWeight, const EDirection aDirection) { // NOLINT
+/// Set a wall into the collision matrix
+void wall(Matrix<Collision>& aCollisions, const Wall& aWall) { // NOLINT
+    // TODO(SRombauts) add those walls into the 8x8 matrix of left/right/up/down walls
+    if (aWall.orientation == 'H') { // 'H' --
+        // x,y-1 x+1,y-1
+        // x,y   x+1,y
+        aCollisions.set(aWall.coords.up())     .bDown = true;
+        aCollisions.set(aWall.coords.upright()).bDown = true;
+        aCollisions.set(aWall.coords)          .bUp   = true;
+        aCollisions.set(aWall.coords.right())  .bUp   = true;
+    } else { // 'V' |
+        // x-1,y   x,y
+        // x-1,y-1 x,y-1
+        aCollisions.set(aWall.coords.left()).bRight     = true;
+        aCollisions.set(aWall.coords.downleft()).bRight = true;
+        aCollisions.set(aWall.coords).bLeft             = true;
+        aCollisions.set(aWall.coords.down()).bLeft      = true;
+    }
+}
+
+/// Recursive shortest path algorithm
+void shortest(Matrix<Cell>& aMatrix, const Coords& aCoords, const float aWeight, const EDirection aDirection) { // NOLINT
     // If the weight ot this path is less than any preceding one on this cell
-    if (aMatrix.get(aX, aY).weight > aWeight) {
+    if (aMatrix.get(aCoords).weight > aWeight) {
         // Update the cell
-        aMatrix.set(aX, aY).weight      = aWeight;
-        aMatrix.set(aX, aY).direction   = aDirection;
+        aMatrix.set(aCoords).weight = aWeight;
+        aMatrix.set(aCoords).direction = aDirection;
 
         // Recurse into adjacent cells
-        if (aX > 0) {
-            shortest(aMatrix, aX - 1, aY, aWeight + 1.0f, eRight);
+        if (aCoords.x > 0) {
+            shortest(aMatrix, aCoords.left(),  aWeight + 1.0f, eRight);
         }
-        if (aX < aMatrix.width() - 1) {
-            shortest(aMatrix, aX + 1, aY, aWeight + 1.0f, eLeft);
+        if (aCoords.x < aMatrix.width() - 1) {
+            shortest(aMatrix, aCoords.right(), aWeight + 1.0f, eLeft);
         }
-        if (aY > 0) {
-            shortest(aMatrix, aX, aY - 1, aWeight + 1.0f, eDown);
+        if (aCoords.y > 0) {
+            shortest(aMatrix, aCoords.up(),    aWeight + 1.0f, eDown);
         }
-        if (aY < aMatrix.height() - 1) {
-            shortest(aMatrix, aX, aY + 1, aWeight + 1.0f, eUp);
+        if (aCoords.y < aMatrix.height() - 1) {
+            shortest(aMatrix, aCoords.down(),  aWeight + 1.0f, eUp);
         }
     }
 }
@@ -202,19 +261,19 @@ void shortest(Matrix<Cell>& aMatrix, const Player& aPlayer) { // NOLINT TODO(SRo
     case 0: {}
         x = 8;
         for (y = 0; y < aMatrix.height() - 1; ++y) {
-            shortest(aMatrix, x, y, weight, eRight);
+            shortest(aMatrix, Coords{ x, y }, weight, eRight);
         }
         break;
     case 1:
         x = 0;
         for (y = 0; y < aMatrix.height() - 1; ++y) {
-            shortest(aMatrix, x, y, weight, eLeft);
+            shortest(aMatrix, Coords{ x, y }, weight, eLeft);
         }
         break;
     case 2:
         y = 8;
         for (x = 0; x < aMatrix.width() - 1; ++x) {
-            shortest(aMatrix, x, y, weight, eDown);
+            shortest(aMatrix, Coords{ x, y }, weight, eDown);
         }
         break;
     default:
@@ -263,14 +322,16 @@ int main() {
 
     // game loop
     while (1) {
-        // TODO(SRombauts) ; a grid by player
-        Matrix<Cell> grid(w, h, Cell{ std::numeric_limits<float>::max(), eRight });
+        // TODO(SRombauts) a grid for pathfinding by player
+        Matrix<Cell> path(w, h, Cell{ std::numeric_limits<float>::max(), eRight });
         for (size_t id = 0; id < playerCount; ++id) {
-            players[id].id          = id; // redundant, but useful for debug
-
             int x; // x-coordinate of the player
             int y; // y-coordinate of the player
-            std::cin >> x >> y >> players[id].wallsLeft; std::cin.ignore();
+            size_t wallsLeft; // number of walls available for the player
+            std::cin >> x >> y >> wallsLeft; std::cin.ignore();
+
+            players[id].id          = id; // redundant, but useful for debug
+            players[id].wallsLeft   = wallsLeft;
 
             // if player still playing
             if ((x >= 0) && (y >= 0)) {
@@ -283,8 +344,8 @@ int main() {
                     std::cerr << "myself(" << players[id].id << "): [" << players[id].coords.x
                               << ", " << players[id].coords.y << "] (left=" << players[id].wallsLeft << ")\n";
                 } else {
-                    // set the player on the grid : 0.0 weight to forbid this cell
-                    grid.set(players[id].coords.x, players[id].coords.y).weight = 0.0;
+                    // set the player on the path : 0.0 weight to forbid this cell
+                    path.set(players[id].coords.x, players[id].coords.y).weight = 0.0;
                     std::cerr << "player(" << players[id].id << "): [" << players[id].coords.x
                               << ", " << players[id].coords.y << "] (left=" << players[id].wallsLeft << ")\n";
                 }
@@ -294,28 +355,27 @@ int main() {
             }
         }
 
-        // debug:
-        dump(grid);
-
         size_t          wallCount; // number of walls on the board
         std::cin >> wallCount; std::cin.ignore();
 
-        Wall::Vector    walls(wallCount);
-        for (size_t wall = 0; wall < wallCount; ++wall) {
-            std::cin >> walls[wall].coords.x >> walls[wall].coords.y >> walls[wall].orientation; std::cin.ignore();
-            std::cerr << "wall(" << wall << "): [" << walls[wall].coords.x << ", " << walls[wall].coords.y << "] '"
-                      << walls[wall].orientation <<"'\n";
+        Wall::Vector        walls(wallCount);
+        Matrix<Collision>   collisions(w, h);
+        for (size_t idx = 0; idx < wallCount; ++idx) {
+            std::cin >> walls[idx].coords.x >> walls[idx].coords.y >> walls[idx].orientation; std::cin.ignore();
+            std::cerr << "idx(" << idx << "): [" << walls[idx].coords.x << ", " << walls[idx].coords.y << "] '"
+                      << walls[idx].orientation <<"'\n";
+            wall(collisions, walls[idx]);
         }
 
         ++turn;
         std::cerr << "turn " << turn << std::endl;
 
         // TODO(SRombauts) test of a pathfinding algorithm:
-        shortest(grid, MySelf);
-        dump(grid);
+        shortest(path, MySelf);
+        dump(path);
 
-        // use the grid to command
-        EDirection bestDirection = grid.get(MySelf.coords.x, MySelf.coords.y).direction;
+        // use the path to command
+        EDirection bestDirection = path.get(MySelf.coords.x, MySelf.coords.y).direction;
         std::cerr << "[" << MySelf.coords.x << ", " << MySelf.coords.y << "]=>" << bestDirection << std::endl;
         Command::move(bestDirection);
 
